854105936bc096807d885cbbf0ab99ac30d2d9ab
Murtaza <alimurtazabhatti0307@gmail.com>
2025-05-15 16:48:10 +0500

    your message

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Work from './components/Work';  // import the Work component
+import React from "react";
+import Work from "./components/Work"; // import the Work component
 
 const App = () => {
   return (
     <div>
       <h1>My React Vite App</h1>
-      <Work />  {/* Render the Work component here */}
+      <Work />
     </div>
   );
 };
