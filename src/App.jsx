2afa64c195f66b73c9aebdfaad99435503493c17
roshanDev-eng <salihabhatti5@gmail.com>
2025-05-15 00:46:26 -0700

    Initial commit of example app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Work from './components/Work';  // import the Work component
+
+const App = () => {
+  return (
+    <div>
+      <h1>My React Vite App</h1>
+      <Work />  {/* Render the Work component here */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Work.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Work =() => {
+    return (
+        <div  style={{ padding: '20px', backgroundColor: '#f0f0f0', borderRadius: '8px' }}>
+  <h2>Work Component</h2>
+      <p>This is a simple Work component for demonstration.</p>
+        </div>
+    )
+};
+export default Work
